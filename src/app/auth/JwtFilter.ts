bc9c78003910fbc27d58895850e98964076c5e9d
Dawid Rzeszotko <rzeszotko.d@gmail.com>
2019-12-15 11:58:57 +0100

    removed log

--- src/app/auth/JwtFilter.ts
+++ src/app/auth/JwtFilter.ts
@@ -9,18 +9,13 @@ export class JwtInterceptor implements HttpInterceptor {
   constructor(private authenticationService: AuthenticationService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('hwtIntercept: intercept');
     const currentUser = this.authenticationService.currentUserValue;
     if (currentUser && currentUser.token) {
-      console.log('current user' + currentUser);
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${currentUser.token}`
         }
       });
-      // request = request.clone({
-      //   headers: request.headers.set('Access-Control-Allow-Origin', '*')
-      // });
     }
     return next.handle(request);
   }
